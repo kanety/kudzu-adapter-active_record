afb199f0f42f00718b61782dee690a2b816f15be
Yoshikazu Kaneta <kaneta@sitebridge.co.jp>
2025-01-01 13:52:51 +0900

    Add load_defaults to dummy app for test

--- spec/dummy/config/application.rb
+++ spec/dummy/config/application.rb
@@ -9,6 +9,8 @@ require "kudzu/adapter/active_record"
 
 module Dummy
   class Application < Rails::Application
+    config.load_defaults Rails::VERSION::STRING.to_f if config.respond_to?(:load_defaults)
+
     unless (database = ENV['DATABASE'].to_s).empty?
       config.paths["config/database"] = "config/database_#{database}.yml"
       ENV['SCHEMA'] = Rails.root.join("db/schema_#{database}.rb").to_s
